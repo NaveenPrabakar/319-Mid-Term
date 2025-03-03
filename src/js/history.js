e42650b6022acbfbc66a7beeccef3ccc1ed2670d
Naveen Prabakar <nvnprabakar@gmail.com>
2025-03-03 00:49:08 -0600

    history

--- src/js/history.js
+++ src/js/history.js
@@ -1,4 +1,3 @@
-
 document.getElementById("vi").addEventListener("click", function () {
     fetch("../assets/history.json").then(response => response.json()).then(myPhones => loadPhones(myPhones))
 });
@@ -10,14 +9,14 @@ function loadPhones(myPhones){
 
     mainContainer.innerHTML = '';
 
-    mainContainer.innerHTML = `
+    mainContainer.innerHTML = 
         <div>
             <h3>${phone.title}</h3>
             <p>${phone.fact}</p>
             <img class= "phone-image" src="${phone.image}" alt="${phone.title}">
             <button class="next-button">Next</button>
         </div>
-    `;
+    ;
 
     mainContainer.querySelector('.next-button').addEventListener("click", function(){
         loadPhone1(myPhones,1);
@@ -32,12 +31,12 @@ function loadPhone1(myPhones, pos){
 
     mainContainer.innerHTML = '';
 
-    mainContainer.innerHTML = `
+    mainContainer.innerHTML = 
         <div>
             <h3>${phone.title}</h3>
             <p>${phone.fact}</p>
             <img class= "phone-image" src="${phone.image}" alt="${phone.title}">
             <button class="next-button">Next</button>
         </div>
-    `;
+    ;
 }
