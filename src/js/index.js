e531a685da26fa77b5c5080f1f55a52706bd126b
Naveen Prabakar <nvnprabakar@gmail.com>
2025-03-02 20:58:12 -0600

    added history page

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
-// script.js
-//Home
+// script.js, this is just to navigate between the pages
+
 document.getElementById("home").addEventListener("click", function () {
     window.location.href = "../html/index.html";  
 });
+
+document.getElementById("history").addEventListener("click", function () {
+    window.location.href = "../html/history.html";  
+});
+
+
+
